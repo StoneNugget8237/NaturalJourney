af055c55c286753ab9119d8e138b91f93f809411
RawDiamondMC <RawDiamondMC@outlook.com>
2024-03-31 21:10:32 +0800

    improve welcome.js

--- hy_farjy[bp]/scripts/hy_farjy/core/welcome.js
+++ hy_farjy[bp]/scripts/hy_farjy/core/welcome.js
@@ -1,13 +1,25 @@
 import { world } from "@minecraft/server";
+import { version } from "../../main";
+import * as semver from "../../dependencies/semver";
 
-world.afterEvents.playerSpawn.subscribe((event)=>{
-    world.setDynamicProperty("ActiveHyfarjyAddonVersion",10001)
-    const ActiveAddonVersion=world.getDynamicProperty("ActiveHyfarjyAddonVersion")
-    const LatestLoadedAddonVersion=world.getDynamicProperty("LatestLoadedHyfarjyAddonVersion")
-    if(LatestLoadedAddonVersion==undefined||LatestLoadedAddonVersion<ActiveAddonVersion){
-      world.setDynamicProperty("LatestLoadedHyfarjyAddonVersion",ActiveAddonVersion)
-    }
-    if(ActiveAddonVersion<LatestLoadedAddonVersion){
-      world.sendMessage(`§c[隐藏之年·更远的旅程]不支持对Addon降级！世界中有Addon版本${Math.floor(LatestLoadedAddonVersion/1000000)}.${Math.floor(LatestLoadedAddonVersion%1000000/10000)}.${Math.floor(LatestLoadedAddonVersion%10000/100)}+build.${Math.floor(LatestLoadedAddonVersion%100)}的加载记录,但当前使用的版本为${Math.floor(ActiveAddonVersion/1000000)}.${Math.floor(ActiveAddonVersion/10000%100)}.${Math.floor(ActiveAddonVersion/100%100)}+build.${Math.floor(ActiveAddonVersion%100)}`)
-    }
-  })
+const latestLoadedHyFarjyVersion =
+  world.getDynamicProperty("LatestLoadedHyfarjyAddonVersion") ?? "0.0.0";
+let warned = false;
+
+world.afterEvents.playerSpawn.subscribe((event) => {
+  if (!event.initialSpawn) {
+    return;
+  }
+  const player = event.player;
+  if (warned || !player.isOp()) {
+    return;
+  }
+  if (semver.lt(latestLoadedHyFarjyVersion, version)) {
+    world.setDynamicProperty("LatestLoadedHyfarjyAddonVersion", version);
+  } else {
+    player.sendMessage(
+      `§c[隐藏之年·更远的旅程]不支持对Addon降级！世界中有Addon版本${latestLoadedHyFarjyVersion}的加载记录,但当前使用的版本为${version}`,
+    );
+  }
+  warned = true;
+});
--- hy_farjy[bp]/scripts/main.js
+++ hy_farjy[bp]/scripts/main.js
@@ -1,3 +1,5 @@
 import "hy_farjy/core/settings.js";
 import "hy_farjy/core/welcome.js";
 import "hy_farjy/gameplay/lottery.js";
+
+export const version = "0.1.0-build.1";
