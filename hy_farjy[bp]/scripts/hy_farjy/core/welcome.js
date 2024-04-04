4b340856c05cf89937d78a4168110872ecdb9e64
StoneNugget <m13644915156@163.com>
2024-04-04 14:06:26 +0800

    bug fix(0.1.0+build.3)

--- hy_farjy[bp]/scripts/hy_farjy/core/welcome.js
+++ hy_farjy[bp]/scripts/hy_farjy/core/welcome.js
@@ -20,6 +20,7 @@ world.afterEvents.playerSpawn.subscribe((event) => {
   }
   if (semver.lt(latestLoadedHyFarjyVersion, version)) {
     world.setDynamicProperty("LatestLoadedHyfarjyAddonVersion", version);
+    world.sendMessage("%hy_farjy.version\n%hy_farjy.update\n%hy_farjy.copyright")
   } else {
     player.sendMessage(
       `§c[隐藏之年·更远的旅程]不支持对Addon降级！世界中有Addon版本${latestLoadedHyFarjyVersion}的加载记录,但当前使用的版本为${version}`,
--- hy_farjy[bp]/scripts/main.js
+++ hy_farjy[bp]/scripts/main.js
@@ -2,4 +2,4 @@ import "hy_farjy/core/settings.js";
 import "hy_farjy/core/welcome.js";
 import "hy_farjy/gameplay/lottery.js";
 
-export const version = "0.1.0-build.1";
+export const version = "0.1.0-build.3";
