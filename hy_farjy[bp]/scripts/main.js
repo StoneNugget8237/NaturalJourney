4490e87114882e6caa681a5efe8999aa6a8d68f7
StoneNugget <m13644915156@163.com>
2024-04-14 19:43:39 +0800

    Enhance Dust&Stable Imitation Swords(0.1.1-build.1)

--- hy_farjy[bp]/scripts/main.js
+++ hy_farjy[bp]/scripts/main.js
@@ -2,4 +2,4 @@ import "hy_farjy/core/settings.js";
 import "hy_farjy/core/welcome.js";
 import "hy_farjy/gameplay/lottery.js";
 
-export const version = "0.1.0-build.3";
+export const version = "0.1.1-build.1";
